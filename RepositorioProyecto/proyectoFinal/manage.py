6091e35c4b50cb3be3a2c5f91bb6fd6e59282049
Camibrbr <camilabritezbritos@gmail.com>
2023-07-15 23:32:31 -0300

    nuevo comment

--- RepositorioProyecto/proyectoFinal/manage.py
+++ RepositorioProyecto/proyectoFinal/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyectofinalprueba.settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyectoFinal.settings.local')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,4 +17,6 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
-main()
+
+if __name__ == '__main__':
+    main()
