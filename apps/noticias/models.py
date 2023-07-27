8aeb658b7906a5c3413b1ecad6c6a8c35fd88b4b
joaquin <joaquin.cleva@gmail.com>
2023-07-27 00:08:23 -0300

    errores en sqlite

--- apps/noticias/models.py
+++ apps/noticias/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+#class diecisieteods(models.Model)
+  #  nom_ods = models.CharField(max_length=80)
+
 class Categoria(models.Model):
     nombre = models.CharField(max_length=80)
     
