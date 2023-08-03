44393d4dca32569b8b6c894111d4fe63f0fd4056
joaquin <joaquin.cleva@gmail.com>
2023-08-03 16:20:26 -0300

    cosas esteticas

--- proyectofinal/settings/local.py
+++ proyectofinal/settings/local.py
@@ -15,6 +15,13 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'grupoinformatorio@gmail.com'
+EMAIL_HOST_PASSWORD = 'yykvqmqfnkjvwftq'
 # # Configuracion de base de datos MySQL
 # DATABASES = {
 #     'default': {
--- proyectofinal/views.py
+++ proyectofinal/views.py
@@ -1,13 +1,46 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.contrib import messages
+
 
 def Hola(request):
-    return render(request,"inicio.html")
+    return render(request, "inicio.html")
+
 
 def Acercade(request):
-    return render(request,"sobre_nosotros.html")
+    return render(request, "sobre_nosotros.html")
+
+
+def Contacto(request):
+    if request.method == "POST":
+        nombre = request.POST.get("nombre")
+        asunto = request.POST.get("asunto")
+        tipo_consulta = request.POST.get("tipo_consulta")
+        mensaje = request.POST.get("mensaje")
+
+        # Enviar correo electrónico
+        subject = f"Nuevo mensaje de contacto - {asunto}"
+        message = f"Nombre: {nombre}\nTipo de consulta: {tipo_consulta}\nMensaje: {mensaje}"
+        from_email = "grupoinformatorio@gmail.com"
+        recipient_list = ["grupoinformatorio@gmail.com"]
+        send_mail(subject, message, from_email, recipient_list)
+        messages.success(request, "El correo ha sido enviado correctamente.")
+
+        return render(request, "contacto.html")
+
+    return render(request, "contacto.html")
+
 
 def RutaConDatos(request):
-    nombres = ["Pedro","Pica","Piedra"]
-    booleanoVerdadero=True
-    booleanoFalso=False
-    return render(request, "rutaConDatos.html",{"nombres":nombres, "booleanoVerdadero":booleanoVerdadero, "booleanoFalso":booleanoFalso})
+    nombres = ["Pedro", "Pica", "Piedra"]
+    booleanoVerdadero = True
+    booleanoFalso = False
+    return render(
+        request,
+        "rutaConDatos.html",
+        {
+            "nombres": nombres,
+            "booleanoVerdadero": booleanoVerdadero,
+            "booleanoFalso": booleanoFalso,
+        },
+    )
